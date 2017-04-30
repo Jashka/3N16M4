3fc7829a7876dc9be205735e7ff3c6deccfd0bd4
Jashka <jacobhenryschaub@gmail.com>
2017-04-30 16:53:53 -0600

    added source code medrotor.java
    from https://codereview.stackexchange.com, to be modified

---
+++ medrotor.java
@@ -0,0 +1,93 @@
+package enigma.rotor;
+
+import enigma.rotor.Rotor;
+
+public class MedRotor extends Rotor {
+    @Override
+    public void setAlphabet()
+    {
+    int i = 0; 
+
+    rotor[i] = ' ';
+    i++;
+
+    rotor[i] = 'E';
+    i++;
+
+    rotor[i] = 'J';
+    i++;
+
+    rotor[i] = 'O';
+    i++;
+
+    rotor[i] = 'T';
+    i++;
+
+    rotor[i] = 'Y';
+    i++;
+
+    rotor[i] = 'C';
+    i++;
+
+    rotor[i] = 'H';
+    i++;
+
+    rotor[i] = 'M';
+    i++;
+
+    rotor[i] = 'R';
+    i++;
+
+    rotor[i] = 'W';
+    i++;
+
+    rotor[i] = 'A';
+    i++;
+
+    rotor[i] = 'F';
+    i++;
+
+    rotor[i] = 'K';
+    i++;
+
+    rotor[i] = 'P';
+    i++;
+
+    rotor[i] = 'U';
+    i++;
+
+    rotor[i] = 'Z';
+    i++;
+
+    rotor[i] = 'D';
+    i++;
+
+    rotor[i] = 'I';
+    i++;
+
+    rotor[i] = 'N';
+    i++;
+
+    rotor[i] = 'S';
+    i++;
+
+    rotor[i] = 'X';
+    i++;
+
+    rotor[i] = 'B';
+    i++;
+
+    rotor[i] = 'G';
+    i++;
+
+    rotor[i] = 'L';
+    i++;
+
+    rotor[i] = 'Q';
+    i++;
+
+    rotor[i] = 'V';
+
+    System.out.println("Finished Initialising Med Rotor  i="+i);
+    }
+}
